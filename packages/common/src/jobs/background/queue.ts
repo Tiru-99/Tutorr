adf0a0fec5da9a691076a924f417ad07168a5513
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-10-02 18:52:36 +0530

    fix : changed the dev script for worker-cron

--- packages/common/src/jobs/background/queue.ts
+++ packages/common/src/jobs/background/queue.ts
@@ -1,6 +1,6 @@
 import { BaseQueue } from "../../bull/BaseQueue";
 import { Redis } from "ioredis";
-import { JobsOptions } from "bullmq";
+
 
 export class BackgroundJobQueue extends BaseQueue<any> {
     constructor(redis: Redis) {
