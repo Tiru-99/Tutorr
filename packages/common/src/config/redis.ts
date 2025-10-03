7d9abc5612f7f39395d4667b80079777f5bc88a2
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-10-03 19:44:11 +0530

    fix : review query fixed and schema changed

--- packages/common/src/config/redis.ts
+++ packages/common/src/config/redis.ts
@@ -3,16 +3,22 @@ import dotenv from 'dotenv'
 
 dotenv.config({ path: '../../.env' })
 
-const redis = new Redis(process.env.REDIS_URL!, {
-    maxRetriesPerRequest: null,
-    tls: {},
-});
+const mode = process.env.NODE_ENV;
 
-// for local docker based development
-// const redis = new Redis({
-//     host : process.env.REDIS_HOST,
-//     port : Number(process.env.REDIS_PORT),
-//     maxRetriesPerRequest : null
-// });
+let redis : Redis; 
+if (mode === "production") {
+    redis = new Redis(process.env.REDIS_URL!, {
+        maxRetriesPerRequest: null,
+        tls: {},
+    });
 
-export default redis; 
+} else {
+    redis = new Redis({
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+        maxRetriesPerRequest: null
+    });
+}
+
+export default redis;
+
