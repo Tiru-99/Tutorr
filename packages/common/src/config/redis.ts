1e6c9fdbc43a8b83ef2eef3d1587da42ceda7260
Tiru-99 <aayushtirmanwar1234@gmail.com>
2025-10-05 01:07:38 +0530

    add : redis config fixed and other changes done as well

--- packages/common/src/config/redis.ts
+++ packages/common/src/config/redis.ts
@@ -2,17 +2,25 @@ import { Redis } from 'ioredis';
 import dotenv from 'dotenv'
 
 dotenv.config({ path: '../../.env' })
+const mode = process.env.NODE_ENV
 
-const redis = new Redis(process.env.REDIS_URL!, {
-    maxRetriesPerRequest: null,
-    tls: {},
-});
+let redis : Redis
+if (mode === "production") {
+    //for prod
+    redis = new Redis(process.env.REDIS_URL!, {
+        maxRetriesPerRequest: null,
+        tls: {},
+    });
+} else {
+    // for dev
+    redis = new Redis({
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+        maxRetriesPerRequest: null
+    });
+}
 
-// for local docker based development
-// const redis = new Redis({
-//     host : process.env.REDIS_HOST,
-//     port : Number(process.env.REDIS_PORT),
-//     maxRetriesPerRequest : null
-// });
+
+
 
 export default redis; 
